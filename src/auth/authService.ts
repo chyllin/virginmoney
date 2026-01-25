b5c709e0062ec44687b2523ef3a96d8f86e3d16d
Maame Esi <esiappeagyei12@gmail.com>
2026-01-25 00:21:59 +0000

    oh my

--- src/auth/authService.ts
+++
@@ -1,7 +0,0 @@
-// src/auth/authService.ts
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase/firebaseAuth";
-
-export const loginUser = (email: string, password: string) => {
-  return signInWithEmailAndPassword(auth, email, password);
-};
