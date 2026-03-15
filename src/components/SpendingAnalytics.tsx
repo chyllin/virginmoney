5bfb7cdb559edb411122326a1081ec67c83471f0
Maame Esi <esiappeagyei12@gmail.com>
2026-03-15 11:58:27 +0000

    £ last

--- src/components/SpendingAnalytics.tsx
+++ src/components/SpendingAnalytics.tsx
@@ -77,7 +77,7 @@ function SpendingAnalytics() {
   const formatCurrency = (amount: number) => {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
-      currency: "EUR",
+      currency: "pound",
       minimumFractionDigits: 2,
     }).format(amount);
   };
